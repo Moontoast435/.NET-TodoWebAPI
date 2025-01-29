acf8b29949cdca20f09b8a613084658506f6b36e
Moontoast435 <seanmalone1993@gmail.com>
2025-01-29 19:49:22 +0000

    Update db context with users table

--- Models/LHQ_SeanContext.cs
+++ Models/LHQ_SeanContext.cs
@@ -2,34 +2,57 @@
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
+using TodoWebAPI;
 
 namespace TodoWebAPI.Models
 {
     public partial class LHQ_SeanContext : DbContext
     {
+        public LHQ_SeanContext()
+        {
+        }
 
         public LHQ_SeanContext(DbContextOptions<LHQ_SeanContext> options)
             : base(options)
         {
         }
 
-        public virtual DbSet<Todo> Todos { get; set; } = null!;
+        public virtual DbSet<User> Users { get; set; } = null!;
+        public DbSet<Todo> Todos { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Data Source=SEANLAPTOP;Initial Catalog=LHQ_Sean;Integrated Security=True");
+                optionsBuilder.UseSqlServer("Data Source=SEANLAPTOP;Initial Catalog=LHQ_Sean;Integrated Security=True;TrustServerCertificate=True");
             }
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.GUserIdx)
+                    .HasColumnName("gUserIdx")
+                    .HasDefaultValueSql("(newid())");
+
+                entity.Property(e => e.SPasswordHash)
+                    .IsUnicode(false)
+                    .HasColumnName("sPasswordHash")
+                    .HasDefaultValueSql("('')");
+
+                entity.Property(e => e.SUsername)
+                    .HasMaxLength(20)
+                    .IsUnicode(false)
+                    .HasColumnName("sUsername")
+                    .HasDefaultValueSql("('')");
+            });
+
             modelBuilder.Entity<Todo>(entity =>
             {
-                entity.ToTable("todos");
-
                 entity.Property(e => e.id).HasColumnName("id");
 
                 entity.Property(e => e.description)
@@ -38,11 +61,9 @@ namespace TodoWebAPI.Models
                     .HasColumnName("description");
 
                 entity.Property(e => e.complete)
-                    .HasColumnName("complete")
-                    .HasColumnType("bit");
+                    .HasColumnName("complete");
 
                 entity.Property(e => e.userId)
-                    .HasColumnType("uniqueidentifier")
                     .HasColumnName("userId");
             });
 
@@ -51,7 +72,4 @@ namespace TodoWebAPI.Models
 
         partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
     }
-
-
-
 }
---
+++ User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TodoWebAPI
+{
+    public partial class User
+    {
+        public int Id { get; set; }
+        public Guid GUserIdx { get; set; }
+        public string? SUsername { get; set; }
+        public string? SPasswordHash { get; set; }
+    }
+}
